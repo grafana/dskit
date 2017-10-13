bf565590330a9fc83c3526b98cd7be053bc8608e
Bryan Boreham <bjboreham@gmail.com>
2017-10-13 16:13:17 +0000

    Move http interceptor into file where it is used, to reduce confusion

--- middleware/logging.go
+++ middleware/logging.go
@@ -1,10 +1,7 @@
 package middleware
 
 import (
-	"bufio"
 	"bytes"
-	"fmt"
-	"net"
 	"net/http"
 	"net/http/httputil"
 	"time"
@@ -57,31 +54,3 @@ func (l Log) Wrap(next http.Handler) http.Handler {
 // Logging middleware logs each HTTP request method, path, response code and
 // duration for all HTTP requests.
 var Logging = Log{}
-
-// interceptor implements WriteHeader to intercept status codes. WriteHeader
-// may not be called on success, so initialize statusCode with the status you
-// want to report on success, i.e. http.StatusOK.
-//
-// interceptor also implements net.Hijacker, to let the downstream Handler
-// hijack the connection. This is needed, for example, for working with websockets.
-type interceptor struct {
-	http.ResponseWriter
-	statusCode int
-	recorded   bool
-}
-
-func (i *interceptor) WriteHeader(code int) {
-	if !i.recorded {
-		i.statusCode = code
-		i.recorded = true
-	}
-	i.ResponseWriter.WriteHeader(code)
-}
-
-func (i *interceptor) Hijack() (net.Conn, *bufio.ReadWriter, error) {
-	hj, ok := i.ResponseWriter.(http.Hijacker)
-	if !ok {
-		return nil, nil, fmt.Errorf("interceptor: can't cast parent ResponseWriter to Hijacker")
-	}
-	return hj.Hijack()
-}

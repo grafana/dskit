e01024f0e161b3f802fbae73fa296fe69c952ada
Tom Wilkie <tomwilkie@users.noreply.github.com>
2018-06-06 13:52:08 +0100

    Add gRPC streaming middleware. (#95)
    * Add gRPC streaming middleware.
    
    Adds a series of gRPC stream middleware for logging, user header propagation and tracing. This is laying groundwork for using gRPC streaming between ingesters and queriers in Cortex.
    
    * Update the vendored versions.
    * Pin kuberesolver because the API has changed.
    
    * Always put code under github.com/weaveworks/common;
    
    Dependancies between packages won't work if the code is checked out in github.com/tomwilkie, for instance.  Also, common is a very common name for a repo, so its not uncommon for the repo to be called something like weaveworks-common.
    
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- server/server.go
+++ server/server.go
@@ -39,9 +39,10 @@ type Config struct {
 	HTTPServerWriteTimeout        time.Duration
 	HTTPServerIdleTimeout         time.Duration
 
-	GRPCOptions    []grpc.ServerOption
-	GRPCMiddleware []grpc.UnaryServerInterceptor
-	HTTPMiddleware []middleware.Interface
+	GRPCOptions          []grpc.ServerOption
+	GRPCMiddleware       []grpc.UnaryServerInterceptor
+	GRPCStreamMiddleware []grpc.StreamServerInterceptor
+	HTTPMiddleware       []middleware.Interface
 }
 
 // RegisterFlags adds the flags required to config this to the given FlagSet
@@ -95,13 +96,24 @@ func New(cfg Config) (*Server, error) {
 	serverLog := middleware.GRPCServerLog{WithRequest: !cfg.ExcludeRequestInLog}
 	grpcMiddleware := []grpc.UnaryServerInterceptor{
 		serverLog.UnaryServerInterceptor,
-		middleware.ServerInstrumentInterceptor(requestDuration),
+		middleware.UnaryServerInstrumentInterceptor(requestDuration),
 		otgrpc.OpenTracingServerInterceptor(opentracing.GlobalTracer()),
 	}
 	grpcMiddleware = append(grpcMiddleware, cfg.GRPCMiddleware...)
+
+	grpcStreamMiddleware := []grpc.StreamServerInterceptor{
+		serverLog.StreamServerInterceptor,
+		middleware.StreamServerInstrumentInterceptor(requestDuration),
+		otgrpc.OpenTracingStreamServerInterceptor(opentracing.GlobalTracer()),
+	}
+	grpcStreamMiddleware = append(grpcStreamMiddleware, cfg.GRPCStreamMiddleware...)
+
 	grpcOptions := []grpc.ServerOption{
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			grpcMiddleware...,
+		)),
+		grpc.StreamInterceptor(grpc_middleware.ChainStreamServer(
+			grpcStreamMiddleware...,
 		)),
 	}
 	grpcOptions = append(grpcOptions, cfg.GRPCOptions...)

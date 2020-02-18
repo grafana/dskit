c033276bbe633460f23952d5ab842779d5d36efe
Bryan Boreham <bjboreham@gmail.com>
2020-02-18 15:27:21 +0000

    Refactor: extract common error decoding

--- middleware/grpc_instrumentation.go
+++ middleware/grpc_instrumentation.go
@@ -10,21 +10,24 @@ import (
 	"google.golang.org/grpc"
 )
 
+func observe(hist *prometheus.HistogramVec, method string, err error, duration time.Duration) {
+	respStatus := "success"
+	if err != nil {
+		if errResp, ok := httpgrpc.HTTPResponseFromError(err); ok {
+			respStatus = strconv.Itoa(int(errResp.Code))
+		} else {
+			respStatus = "error"
+		}
+	}
+	hist.WithLabelValues(gRPC, method, respStatus, "false").Observe(duration.Seconds())
+}
+
 // UnaryServerInstrumentInterceptor instruments gRPC requests for errors and latency.
 func UnaryServerInstrumentInterceptor(hist *prometheus.HistogramVec) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
 		begin := time.Now()
 		resp, err := handler(ctx, req)
-		duration := time.Since(begin).Seconds()
-		respStatus := "success"
-		if err != nil {
-			if errResp, ok := httpgrpc.HTTPResponseFromError(err); ok {
-				respStatus = strconv.Itoa(int(errResp.Code))
-			} else {
-				respStatus = "error"
-			}
-		}
-		hist.WithLabelValues(gRPC, info.FullMethod, respStatus, "false").Observe(duration)
+		observe(hist, info.FullMethod, err, time.Since(begin))
 		return resp, err
 	}
 }
@@ -34,16 +37,7 @@ func StreamServerInstrumentInterceptor(hist *prometheus.HistogramVec) grpc.Strea
 	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
 		begin := time.Now()
 		err := handler(srv, ss)
-		duration := time.Since(begin).Seconds()
-		respStatus := "success"
-		if err != nil {
-			if errResp, ok := httpgrpc.HTTPResponseFromError(err); ok {
-				respStatus = strconv.Itoa(int(errResp.Code))
-			} else {
-				respStatus = "error"
-			}
-		}
-		hist.WithLabelValues(gRPC, info.FullMethod, respStatus, "false").Observe(duration)
+		observe(hist, info.FullMethod, err, time.Since(begin))
 		return err
 	}
 }

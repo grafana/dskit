d8a39b93ceaff14fee98a273e3e18e133e74a790
Tyler Reid <tyler.reid@grafana.com>
2021-08-10 09:56:06 -0500

    Move backoff from util/backoff to dskit/backoff. Rename backoff config to config for clarity
    Signed-off-by: Tyler Reid <tyler.reid@grafana.com>

--- backoff/backoff_test.go
+++ backoff/backoff_test.go
@@ -1,4 +1,4 @@
-package util
+package backoff
 
 import (
 	"context"
@@ -85,7 +85,7 @@ func TestBackoff_NextDelay(t *testing.T) {
 		t.Run(testName, func(t *testing.T) {
 			t.Parallel()
 
-			b := NewBackoff(context.Background(), BackoffConfig{
+			b := NewBackoff(context.Background(), Config{
 				MinBackoff: testData.minBackoff,
 				MaxBackoff: testData.maxBackoff,
 				MaxRetries: len(testData.expectedRanges),

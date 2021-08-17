988065d952d54288bd1c4d551cb5c3e142c5dade
Nick Pillitteri <nick.pillitteri@grafana.com>
2021-08-17 12:24:56 -0400

    Changes to work within dskit
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- modules/modules_test.go
+++ modules/modules_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/cortexproject/cortex/pkg/util/services"
+	"github.com/grafana/dskit/services"
 )
 
 func mockInitFunc() (services.Service, error) { return services.NewIdleService(nil, nil), nil }

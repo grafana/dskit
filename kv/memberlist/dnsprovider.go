71ff42c32e22330c03011f7344c98c62da16a12f
Arve Knudsen <arve.knudsen@gmail.com>
2021-08-25 08:09:05 +0200

    Document DNSProvider interface
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- kv/memberlist/dnsprovider.go
+++ kv/memberlist/dnsprovider.go
@@ -4,6 +4,7 @@ import (
 	"context"
 )
 
+// DNSProvider supports storing or resolving a list of addresses.
 type DNSProvider interface {
 	// Resolve stores a list of provided addresses or their DNS records if requested.
 	// Addresses prefixed with `dns+` or `dnssrv+` will be resolved through respective DNS lookup (A/AAAA or SRV).

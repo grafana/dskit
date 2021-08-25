5303e8e8be77601b73b4331fd0d9a2b301b81123
Arve Knudsen <arve.knudsen@gmail.com>
2021-08-25 09:23:46 +0200

    Add DNSProvider example
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- kv/memberlist/dnsprovider.go
+++ kv/memberlist/dnsprovider.go
@@ -9,6 +9,8 @@ type DNSProvider interface {
 	// Resolve stores a list of provided addresses or their DNS records if requested.
 	// Addresses prefixed with `dns+` or `dnssrv+` will be resolved through respective DNS lookup (A/AAAA or SRV).
 	// For non-SRV records, it will return an error if a port is not supplied.
+	//
+	// Example: By providing the address of "dns+grafana.com:80", it should resolve into something like []string{"34.120.177.193:80", "[2600:1901:0:b3ea::]:80"}.
 	Resolve(ctx context.Context, addrs []string) error
 
 	// Addresses returns the latest addresses present in the DNSProvider.

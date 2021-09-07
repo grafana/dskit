301d0151196b7340d8d9bf230691745dec18ca62
Tyler Reid <tyler.reid@grafana.com>
2021-09-07 08:57:42 -0500

    Update poll description
    Signed-off-by: Tyler Reid <tyler.reid@grafana.com>

--- test/poll.go
+++ test/poll.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-// Poll repeatedly evaluates condition until we either timeout, or it succeeds.
+// Poll repeatedly calls a function until the function returns the correct response or until poll timeout.
 func Poll(t testing.TB, d time.Duration, want interface{}, have func() interface{}) {
 	t.Helper()
 	deadline := time.Now().Add(d)

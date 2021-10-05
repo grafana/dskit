80acac39018cf4bf3ed0e8d773880e007ca2cd3b
Tyler Reid <tyler.reid@grafana.com>
2021-10-05 16:11:21 -0500

    Refactor global metrics in ring to use a gauge and a ticker to update the gauges

--- ring/lifecycler_metrics.go
+++ ring/lifecycler_metrics.go
@@ -20,19 +20,19 @@ func NewLifecyclerMetrics(ringName string, reg prometheus.Registerer) *Lifecycle
 			ConstLabels: prometheus.Labels{"name": ringName},
 		}),
 		tokensOwned: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-			Name: "member_ring_tokens_owned",
-			Help: "The number of tokens owned in the ring.",
+			Name:        "member_ring_tokens_owned",
+			Help:        "The number of tokens owned in the ring.",
 			ConstLabels: prometheus.Labels{"name": ringName},
 		}),
 		tokensToOwn: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
-			Name: "member_ring_tokens_to_own",
-			Help: "The number of tokens to own in the ring.",
+			Name:        "member_ring_tokens_to_own",
+			Help:        "The number of tokens to own in the ring.",
 			ConstLabels: prometheus.Labels{"name": ringName},
 		}),
 		shutdownDuration: promauto.With(reg).NewHistogramVec(prometheus.HistogramOpts{
-			Name:    "shutdown_duration_seconds",
-			Help:    "Duration (in seconds) of shutdown procedure (ie transfer or flush).",
-			Buckets: prometheus.ExponentialBuckets(10, 2, 8), // Biggest bucket is 10*2^(9-1) = 2560, or 42 mins.
+			Name:        "shutdown_duration_seconds",
+			Help:        "Duration (in seconds) of shutdown procedure (ie transfer or flush).",
+			Buckets:     prometheus.ExponentialBuckets(10, 2, 8), // Biggest bucket is 10*2^(9-1) = 2560, or 42 mins.
 			ConstLabels: prometheus.Labels{"name": ringName},
 		}, []string{"op", "status", "name"}),
 	}

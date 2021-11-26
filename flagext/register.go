73b8988f8dc8f62d906a426cad1330b62cb69e32
Bryan Boreham <bjboreham@gmail.com>
2021-11-26 18:47:09 +0000

    flagext: add RegisterFlagsWithLogger
    For cases such as `DeprecatedFlag()` that need a logger, extend
    `RegisterFlags` to allow this while still working with existing code.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- flagext/register.go
+++ flagext/register.go
@@ -1,10 +1,19 @@
 package flagext
 
-import "flag"
+import (
+	"flag"
+
+	"github.com/go-kit/log"
+)
 
 // Registerer is a thing that can RegisterFlags
 type Registerer interface {
 	RegisterFlags(*flag.FlagSet)
+}
+
+// Registerer2 is a thing that can RegisterFlags with a Logger
+type Registerer2 interface {
+	RegisterFlags(*flag.FlagSet, log.Logger)
 }
 
 // RegisterFlags registers flags with the provided Registerers
@@ -14,11 +23,33 @@ func RegisterFlags(rs ...Registerer) {
 	}
 }
 
+// RegisterFlagsWithLogger registers flags with the provided Registerers
+func RegisterFlagsWithLogger(logger log.Logger, rs ...interface{}) {
+	for _, v := range rs {
+		switch r := v.(type) {
+		case Registerer:
+			r.RegisterFlags(flag.CommandLine)
+		case Registerer2:
+			r.RegisterFlags(flag.CommandLine, logger)
+		default:
+			panic("RegisterFlagsWithLogger must be passed a Registerer")
+		}
+	}
+}
+
 // DefaultValues initiates a set of configs (Registerers) with their defaults.
-func DefaultValues(rs ...Registerer) {
+func DefaultValues(rs ...interface{}) {
 	fs := flag.NewFlagSet("", flag.PanicOnError)
-	for _, r := range rs {
-		r.RegisterFlags(fs)
+	logger := log.NewNopLogger()
+	for _, v := range rs {
+		switch r := v.(type) {
+		case Registerer:
+			r.RegisterFlags(fs)
+		case Registerer2:
+			r.RegisterFlags(fs, logger)
+		default:
+			panic("RegisterFlagsWithLogger must be passed a Registerer")
+		}
 	}
 	_ = fs.Parse([]string{})
 }

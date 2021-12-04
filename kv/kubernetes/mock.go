24d680237744956706389f495ebc28dbb883a0e0
Christian Simon <simon@swine.de>
2021-12-04 09:54:29 +0000

    Ensure restclient is populated

--- kv/kubernetes/mock.go
+++ kv/kubernetes/mock.go
@@ -8,10 +8,29 @@ import (
 	"github.com/grafana/dskit/kv/codec"
 
 	"k8s.io/client-go/kubernetes/fake"
+	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+	fake_rest "k8s.io/client-go/rest/fake"
 )
 
+type fakeClientset struct {
+	*fake.Clientset
+}
+
+func (c fakeClientset) CoreV1() corev1.CoreV1Interface {
+	return fakeCoreV1{c.Clientset.CoreV1()}
+}
+
+type fakeCoreV1 struct {
+	corev1.CoreV1Interface
+}
+
+func (c fakeCoreV1) RESTClient() rest.Interface {
+	return &fake_rest.RESTClient{}
+}
+
 func NewInMemoryClient(codec codec.Codec, logger log.Logger) (*Client, io.Closer) {
-	fakeClientset := fake.NewSimpleClientset()
+	fakeClientset := fakeClientset{fake.NewSimpleClientset()}
 
 	client, err := newClient(&Config{}, codec, logger, nil, func(c *Client) error {
 		c.clientset = fakeClientset

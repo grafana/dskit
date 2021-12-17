469be604e892a4c5757bdfd2c7525ecfbcb59dc5
Christian Haudum <christian.haudum@gmail.com>
2021-12-17 16:18:26 +0100

    fixup! Add netutil package
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- netutil/netutil.go
+++ netutil/netutil.go
@@ -39,7 +39,7 @@ IFACES:
 				if !ok {
 					continue
 				}
-				if !ip.IsPrivate() {
+				if !IsPrivate(ip) {
 					continue IFACES
 				}
 			}
@@ -105,4 +105,25 @@ func filterIPs(addrs []net.Addr) net.IP {
 		}
 	}
 	return ipAddr
+}
+
+// IsPrivate reports whether ip is a private address, according to
+// RFC 1918 (IPv4 addresses) and RFC 4193 (IPv6 addresses).
+// Copied form net package of the Go 1.17 stdlib.
+// So, it can be removed once dskit is updated to Go 1.17
+func IsPrivate(ip net.IP) bool {
+	if ip4 := ip.To4(); ip4 != nil {
+		// Following RFC 1918, Section 3. Private Address Space which says:
+		//   The Internet Assigned Numbers Authority (IANA) has reserved the
+		//   following three blocks of the IP address space for private internets:
+		//     10.0.0.0        -   10.255.255.255  (10/8 prefix)
+		//     172.16.0.0      -   172.31.255.255  (172.16/12 prefix)
+		//     192.168.0.0     -   192.168.255.255 (192.168/16 prefix)
+		return ip4[0] == 10 ||
+			(ip4[0] == 172 && ip4[1]&0xf0 == 16) ||
+			(ip4[0] == 192 && ip4[1] == 168)
+	}
+	// Following RFC 4193, Section 8. IANA Considerations which says:
+	//   The IANA has assigned the FC00::/7 prefix to "Unique Local Unicast".
+	return len(ip) == net.IPv6len && ip[0]&0xfe == 0xfc
 }

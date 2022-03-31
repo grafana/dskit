6d48ca07ade59022d5e1467c7f72cbfa01aecfa6
Peter Štibraný <pstibrany@gmail.com>
2022-03-31 12:05:36 +0200

    Multikv: Register metrics before starting watcher goroutine. (#153)
    * Multikv: Register metrics before starting watcher goroutine.
    
    * CHANGELOG.md

--- kv/multi_test.go
+++ kv/multi_test.go
@@ -3,6 +3,7 @@ package kv
 import (
 	"testing"
 
+	"github.com/go-kit/log"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v2"
 )
@@ -29,4 +30,20 @@ func TestMultiRuntimeConfigWithVariousEnabledValues(t *testing.T) {
 			assert.Equal(t, tc.expected, c.Mirroring, tc.yaml)
 		})
 	}
+}
+
+// This simple test could reproduce panic in watchConfigChannel goroutine, if metrics didn't exist yet.
+func TestNewMulti(t *testing.T) {
+	cfg := MultiConfig{ConfigProvider: func() <-chan MultiRuntimeConfig {
+		ch := make(chan MultiRuntimeConfig, 1)
+		ch <- MultiRuntimeConfig{
+			PrimaryStore: "b",
+		}
+		return ch
+	}}
+
+	clients := []kvclient{{name: "a"}, {name: "b"}}
+
+	mc := NewMultiClient(cfg, clients, log.NewNopLogger(), nil)
+	mc.cancel()
 }

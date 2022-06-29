3690084ebccc2820775c7a37da73c361f3b7c7b9
Peter Štibraný <pstibrany@gmail.com>
2022-06-29 09:47:59 +0200

    Optimize lookback when all Registered Timestamps are within the lookback window.

--- ring/ring.go
+++ ring/ring.go
@@ -167,6 +167,10 @@ type Ring struct {
 	ringTokens       []uint32
 	ringTokensByZone map[string][]uint32
 
+	// Oldest value of RegisteredTimestamp from all instances. If any instance had RegisteredTimestamp == 0,
+	// then this value will be 0.
+	oldestRegisteredTimestamp int64
+
 	// Maps a token with the information of the instance holding it. This map is immutable and
 	// cannot be chanced in place because it's shared "as is" between subrings (the only way to
 	// change it is to create a new one and replace it).
@@ -310,6 +314,7 @@ func (r *Ring) updateRingState(ringDesc *Desc) {
 	ringTokensByZone := ringDesc.getTokensByZone()
 	ringInstanceByToken := ringDesc.getTokensInfo()
 	ringZones := getZones(ringTokensByZone)
+	oldestRegisteredTimestamp := ringDesc.getOldestRegisteredTimestamp()
 
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
@@ -318,6 +323,7 @@ func (r *Ring) updateRingState(ringDesc *Desc) {
 	r.ringTokensByZone = ringTokensByZone
 	r.ringInstanceByToken = ringInstanceByToken
 	r.ringZones = ringZones
+	r.oldestRegisteredTimestamp = oldestRegisteredTimestamp
 	r.lastTopologyChange = now
 	if r.shuffledSubringCache != nil {
 		// Invalidate all cached subrings.
@@ -630,6 +636,16 @@ func (r *Ring) shuffleShard(identifier string, size int, lookbackPeriod time.Dur
 	r.mtx.RLock()
 	defer r.mtx.RUnlock()
 
+	// If all instances have RegisteredTimestamp within the lookback period,
+	// then all instances would be included in the resulting ring, so we can
+	// simply return this ring.
+	//
+	// If any instance had RegisteredTimestamp equal to 0 (it would not cause additional lookup of next instance),
+	// then r.oldestRegisteredTimestamp is zero too, and we skip this optimization.
+	if r.oldestRegisteredTimestamp > 0 && r.oldestRegisteredTimestamp >= lookbackUntil {
+		return r
+	}
+
 	var numInstancesPerZone int
 	var actualZones []string
 
@@ -725,6 +741,8 @@ func (r *Ring) shuffleShard(identifier string, size int, lookbackPeriod time.Dur
 		ringTokens:       shardDesc.GetTokens(),
 		ringTokensByZone: shardTokensByZone,
 		ringZones:        getZones(shardTokensByZone),
+
+		oldestRegisteredTimestamp: shardDesc.getOldestRegisteredTimestamp(),
 
 		// We reference the original map as is in order to avoid copying. It's safe to do
 		// because this map is immutable by design and it's a superset of the actual instances

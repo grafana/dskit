e414922a81f282d4d731b620cb2c6e141414cd8c
Bryan Boreham <bjboreham@gmail.com>
2022-08-31 10:36:37 +0100

    runtutil: add ExhaustCloseWithErrCapture (#210)
    * runtutil: add ExhaustCloseWithErrCapture
    
    Code from https://github.com/thanos-io/thanos/blob/6d1b98d49781/pkg/runutil/,
    slightly modified for consistency.
    
    This enables grafana/e2e to build without a dependency on Thanos.
    
    * Improve test style
    
    Use assert/require where possible.
    
    Supply a name to t.Run().
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- runutil/runutil_test.go
+++ runutil/runutil_test.go
@@ -1,12 +1,20 @@
+// Provenance-includes-location: https://github.com/thanos-io/thanos/blob/main/pkg/runutil/runutil_test.go
+// Provenance-includes-license: Apache-2.0
+// Provenance-includes-copyright: The Thanos Authors.
+
 package runutil
 
 import (
 	"fmt"
+	"io"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/go-kit/log/level"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCloseWithLogOnErr(t *testing.T) {
@@ -55,4 +63,95 @@ type fakeLogger struct {
 func (l *fakeLogger) Log(keyvals ...interface{}) error {
 	l.keyvals = keyvals
 	return nil
+}
+
+type testCloser = fakeCloser
+
+func TestCloseWithErrCapture(t *testing.T) {
+	for _, tcase := range []struct {
+		err    error
+		closer io.Closer
+
+		expectedErrStr string
+	}{
+		{
+			err:            nil,
+			closer:         testCloser{err: nil},
+			expectedErrStr: "",
+		},
+		{
+			err:            errors.New("test"),
+			closer:         testCloser{err: nil},
+			expectedErrStr: "test",
+		},
+		{
+			err:            nil,
+			closer:         testCloser{err: errors.New("test")},
+			expectedErrStr: "close: test",
+		},
+		{
+			err:            errors.New("test"),
+			closer:         testCloser{err: errors.New("test")},
+			expectedErrStr: "2 errors: test; close: test",
+		},
+	} {
+		t.Run(tcase.expectedErrStr, func(t *testing.T) {
+			ret := tcase.err
+			CloseWithErrCapture(&ret, tcase.closer, "close")
+
+			if tcase.expectedErrStr == "" {
+				assert.NoError(t, ret)
+			} else {
+				require.Error(t, ret)
+				assert.Equal(t, tcase.expectedErrStr, ret.Error())
+			}
+		})
+	}
+}
+
+type loggerCapturer struct {
+	// WasCalled is true if the Log() function has been called.
+	WasCalled bool
+}
+
+func (lc *loggerCapturer) Log(keyvals ...interface{}) error {
+	lc.WasCalled = true
+	return nil
+}
+
+type emulatedCloser struct {
+	io.Reader
+
+	calls int
+}
+
+func (e *emulatedCloser) Close() error {
+	e.calls++
+	if e.calls == 1 {
+		return nil
+	}
+	if e.calls == 2 {
+		return errors.Wrap(os.ErrClosed, "can even be a wrapped one")
+	}
+	return errors.New("something very bad happened")
+}
+
+// newEmulatedCloser returns a ReadCloser with a Close method
+// that at first returns success but then returns that
+// it has been closed already. After that, it returns that
+// something very bad had happened.
+func newEmulatedCloser(r io.Reader) io.ReadCloser {
+	return &emulatedCloser{Reader: r}
+}
+
+func TestCloseMoreThanOnce(t *testing.T) {
+	lc := &loggerCapturer{}
+	r := newEmulatedCloser(strings.NewReader("somestring"))
+
+	CloseWithLogOnErr(lc, r, "should not be called")
+	CloseWithLogOnErr(lc, r, "should not be called")
+	assert.False(t, lc.WasCalled)
+
+	CloseWithLogOnErr(lc, r, "should be called")
+	assert.True(t, lc.WasCalled)
 }

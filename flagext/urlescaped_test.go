6b8c28bc192ec4f3b90aa7443338750a16c47b01
Kaviraj <kavirajkanagaraj@gmail.com>
2023-02-07 22:22:44 +0100

    flagext: Add new type `URLEscaped`
    Sometimes we need to store just escaped URL in the config like S3 URL that contains credentials which can include characters like
    `:` and `/`. Currently we cannot use normal `flagext.URLValue` as it just uses unescaped URL.
    
    Please check issue
    https://github.com/grafana/loki/issues/1607

---
+++ flagext/urlescaped_test.go
@@ -0,0 +1,38 @@
+package flagext
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	testS3URL = "s3://ASDFGHJIQWETTYUI:Jkasduahdkjh213kj1h31+lkjaflkjzvKASDOasofhjafaKFAF/GoQd@region/bucket_name"
+)
+
+func TestURLEscaped(t *testing.T) {
+	expected, err := url.Parse(url.QueryEscape(testS3URL))
+	require.NoError(t, err)
+
+	// flag
+	var v URLEscaped
+	flags := flag.NewFlagSet("test", flag.ExitOnError)
+	flags.Var(&v, "v", "some secret credentials")
+	err = flags.Parse([]string{"-v", testS3URL})
+
+	assert.Equal(t, v.String(), expected.String())
+
+	// yaml
+	yv := struct {
+		S3 URLEscaped `yaml:"s3"`
+	}{}
+	err = yaml.Unmarshal([]byte(fmt.Sprintf("s3: %s", testS3URL)), &yv)
+	assert.NoError(t, err)
+	assert.Equal(t, yv.S3.String(), expected.String())
+
+}

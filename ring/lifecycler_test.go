6d71bb1a3073e01b3ab9c84bf83e0b48c16153b1
sarthak.tyagi <sarthaktyagi100@gmail.com>
2023-04-29 03:03:53 +0100

    ring.Lifecycler: Handle when previous ring state is leaving and the number of tokens has changed

--- ring/lifecycler_test.go
+++ ring/lifecycler_test.go
@@ -1422,6 +1422,6 @@ func GenerateTokenJSONFile(tokens int) string {
 		}
 		tokens--
 	}
-	token, _ := json.Marshal(&tokenFile)
-	return string(token)
+	tokenFileJSON, _ := json.Marshal(&tokenFile)
+	return string(tokenFileJSON)
 }

797811ef1d1986d228824ffc03edd5bd23d523ee
DylanGuedes <djmgguedes@gmail.com>
2023-11-15 08:51:08 -0300

    try new span change

--- middleware/http_tracing.go
+++ middleware/http_tracing.go
@@ -114,6 +114,7 @@ func (hgt HTTPGRPCTracer) Wrap(next http.Handler) http.Handler {
 			opentracing.Tag{Key: string(ext.HTTPMethod), Value: method},
 			opentracing.Tag{Key: "http.route", Value: matchedRoute},
 			opentracing.Tag{Key: "http.user_agent", Value: userAgent},
+			opentracing.Tag{Key: "root_action", Value: childSpanName},
 		}
 		if parentSpan != nil {
 			startSpanOpts = append(
@@ -125,6 +126,7 @@ func (hgt HTTPGRPCTracer) Wrap(next http.Handler) http.Handler {
 		}
 
 		childSpan := tracer.StartSpan(childSpanName, startSpanOpts...)
+		childSpan = childSpan.SetBaggageItem("root_action", childSpanName)
 		defer childSpan.Finish()
 
 		r = r.WithContext(opentracing.ContextWithSpan(r.Context(), childSpan))

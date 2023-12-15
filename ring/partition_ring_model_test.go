ef3d7e26171a1ca51f7c8005fdc3f73725170a39
Peter Štibraný <pstibrany@gmail.com>
2023-12-15 17:32:28 +0100

    Fix test.

--- ring/partition_ring_model_test.go
+++ ring/partition_ring_model_test.go
@@ -11,23 +11,24 @@ func TestPartitionDesc_State(t *testing.T) {
 	type testcase struct {
 		active, inactive, deleted int64
 		expectedState             PartitionState
+		expectedTimestamp         int64
 	}
 
 	for _, tc := range []testcase{
-		{active: 0, inactive: 0, deleted: 0, expectedState: PartitionActive},
-		{active: 10, inactive: 0, deleted: 0, expectedState: PartitionActive},
-		{active: 10, inactive: 10, deleted: 0, expectedState: PartitionActive},
-		{active: 10, inactive: 10, deleted: 10, expectedState: PartitionActive},
-		{active: 10, inactive: 5, deleted: 10, expectedState: PartitionActive},
-		{active: 10, inactive: 5, deleted: 5, expectedState: PartitionActive},
-		{active: 0, inactive: 10, deleted: 0, expectedState: PartitionInactive},
-		{active: 0, inactive: 10, deleted: 10, expectedState: PartitionInactive},
-		{active: 5, inactive: 10, deleted: 10, expectedState: PartitionInactive},
-		{active: 5, inactive: 10, deleted: 0, expectedState: PartitionInactive},
-		{active: 0, inactive: 0, deleted: 10, expectedState: PartitionDeleted},
-		{active: 0, inactive: 5, deleted: 10, expectedState: PartitionDeleted},
-		{active: 5, inactive: 0, deleted: 10, expectedState: PartitionDeleted},
-		{active: 5, inactive: 5, deleted: 10, expectedState: PartitionDeleted},
+		{active: 0, inactive: 0, deleted: 0, expectedState: PartitionActive, expectedTimestamp: 0},
+		{active: 10, inactive: 0, deleted: 0, expectedState: PartitionActive, expectedTimestamp: 10},
+		{active: 10, inactive: 10, deleted: 0, expectedState: PartitionActive, expectedTimestamp: 10},
+		{active: 10, inactive: 10, deleted: 10, expectedState: PartitionActive, expectedTimestamp: 10},
+		{active: 10, inactive: 5, deleted: 10, expectedState: PartitionActive, expectedTimestamp: 10},
+		{active: 10, inactive: 5, deleted: 5, expectedState: PartitionActive, expectedTimestamp: 10},
+		{active: 0, inactive: 10, deleted: 0, expectedState: PartitionInactive, expectedTimestamp: 10},
+		{active: 0, inactive: 10, deleted: 10, expectedState: PartitionInactive, expectedTimestamp: 10},
+		{active: 5, inactive: 10, deleted: 10, expectedState: PartitionInactive, expectedTimestamp: 10},
+		{active: 5, inactive: 10, deleted: 0, expectedState: PartitionInactive, expectedTimestamp: 10},
+		{active: 0, inactive: 0, deleted: 10, expectedState: PartitionDeleted, expectedTimestamp: 10},
+		{active: 0, inactive: 5, deleted: 10, expectedState: PartitionDeleted, expectedTimestamp: 10},
+		{active: 5, inactive: 0, deleted: 10, expectedState: PartitionDeleted, expectedTimestamp: 10},
+		{active: 5, inactive: 5, deleted: 10, expectedState: PartitionDeleted, expectedTimestamp: 10},
 	} {
 		t.Run(fmt.Sprintf("%d_%d_%d", tc.active, tc.inactive, tc.deleted), func(t *testing.T) {
 			desc := PartitionDesc{
@@ -36,7 +37,9 @@ func TestPartitionDesc_State(t *testing.T) {
 				DeletedSince:  tc.deleted,
 			}
 
-			require.Equal(t, tc.expectedState, desc.State())
+			s, ts := desc.State()
+			require.Equal(t, tc.expectedState, s)
+			require.Equal(t, tc.expectedTimestamp, ts)
 		})
 	}
 }

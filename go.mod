3cbc46dba7e38ab63831b06c0ddd128e6462525e
Marco Pracucci <marco@pracucci.com>
2024-02-14 09:14:59 +0100

    Display corrupted partitions in ring UI page
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- go.mod
+++ go.mod
@@ -57,7 +57,9 @@ require (
 
 require (
 	github.com/OneOfOne/xxhash v1.2.6 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
 	github.com/alicebob/gopher-json v0.0.0-20200520072559-a9ecdc9d1d3a // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/codahale/hdrhistogram v0.0.0-20161010025455-3a0bb77429bd // indirect
--- ring/partition_ring_http_test.go
+++ ring/partition_ring_http_test.go
@@ -4,10 +4,13 @@ import (
 	_ "embed"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestPartitionRingPageHandler(t *testing.T) {
@@ -37,6 +40,11 @@ func TestPartitionRingPageHandler(t *testing.T) {
 					"ingester-zone-b-1": {
 						OwnedPartition: 2,
 					},
+
+					// Simulate a corrupted partition, with a dangling owner but no partition.
+					"ingester-zone-b-2": {
+						OwnedPartition: 3,
+					},
 				},
 			}),
 		),
@@ -47,4 +55,30 @@ func TestPartitionRingPageHandler(t *testing.T) {
 
 	assert.Equal(t, http.StatusOK, recorder.Code)
 	assert.Equal(t, "text/html", recorder.Header().Get("Content-Type"))
+
+	doc, err := goquery.NewDocumentFromReader(recorder.Body)
+	require.NoError(t, err)
+
+	rows := doc.Find("table tbody tr")
+	require.Equal(t, 3, rows.Length())
+
+	row := doc.Find("table tbody tr:nth-child(1)")
+	require.Equal(t, 1, row.Length())
+	assert.Equal(t, "1", strings.TrimSpace(row.Find("td:nth-child(1)").Text()))
+	assert.Equal(t, "Active", strings.TrimSpace(row.Find("td:nth-child(2)").Text()))
+	assert.Contains(t, strings.TrimSpace(row.Find("td:nth-child(4)").Text()), "ingester-zone-a-0")
+	assert.Contains(t, strings.TrimSpace(row.Find("td:nth-child(4)").Text()), "ingester-zone-b-0")
+
+	row = doc.Find("table tbody tr:nth-child(2)")
+	require.Equal(t, 1, row.Length())
+	assert.Equal(t, "2", strings.TrimSpace(row.Find("td:nth-child(1)").Text()))
+	assert.Equal(t, "Inactive", strings.TrimSpace(row.Find("td:nth-child(2)").Text()))
+	assert.Contains(t, strings.TrimSpace(row.Find("td:nth-child(4)").Text()), "ingester-zone-a-1")
+	assert.Contains(t, strings.TrimSpace(row.Find("td:nth-child(4)").Text()), "ingester-zone-b-1")
+
+	row = doc.Find("table tbody tr:nth-child(3)")
+	require.Equal(t, 1, row.Length())
+	assert.Equal(t, "3", strings.TrimSpace(row.Find("td:nth-child(1)").Text()))
+	assert.Equal(t, "Corrupted", strings.TrimSpace(row.Find("td:nth-child(2)").Text()))
+	assert.Contains(t, strings.TrimSpace(row.Find("td:nth-child(4)").Text()), "ingester-zone-b-2")
 }

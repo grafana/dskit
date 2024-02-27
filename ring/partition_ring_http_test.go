ffb15fd72381eef80bce6d406d081058c45b2359
Marco Pracucci <marco@pracucci.com>
2024-02-27 15:37:01 +0100

    Add option to force partition state change from UI (#497)
    * Add option to force partition state change from UI
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    * Extract duplicated code in changePartitionState()
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>
    
    ---------
    
    Signed-off-by: Marco Pracucci <marco@pracucci.com>

--- ring/partition_ring_http_test.go
+++ ring/partition_ring_http_test.go
@@ -1,19 +1,26 @@
 package ring
 
 import (
+	"context"
 	_ "embed"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"regexp"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/go-kit/log"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/grafana/dskit/kv/consul"
+	"github.com/grafana/dskit/services"
 )
 
-func TestPartitionRingPageHandler(t *testing.T) {
+func TestPartitionRingPageHandler_ViewPage(t *testing.T) {
 	handler := NewPartitionRingPageHandler(
 		newStaticPartitionRingReader(
 			NewPartitionRing(PartitionRingDesc{
@@ -48,6 +55,7 @@ func TestPartitionRingPageHandler(t *testing.T) {
 				},
 			}),
 		),
+		nil,
 	)
 
 	recorder := httptest.NewRecorder()
@@ -76,4 +84,117 @@ func TestPartitionRingPageHandler(t *testing.T) {
 		"<td>", "N/A", "</td>",
 		"<td>", "ingester-zone-b-2", "<br />", "</td>",
 	}, `\s*`))), recorder.Body.String())
+}
+
+func TestPartitionRingPageHandler_ChangePartitionState(t *testing.T) {
+	ctx := context.Background()
+	logger := log.NewNopLogger()
+
+	store, closer := consul.NewInMemoryClient(GetPartitionRingCodec(), log.NewNopLogger(), nil)
+	t.Cleanup(func() { assert.NoError(t, closer.Close()) })
+
+	// Init the ring.
+	require.NoError(t, store.CAS(ctx, ringKey, func(in interface{}) (out interface{}, retry bool, err error) {
+		desc := GetOrCreatePartitionRingDesc(in)
+		desc.AddPartition(1, PartitionActive, time.Now())
+		desc.AddPartition(2, PartitionActive, time.Now())
+		return desc, true, nil
+	}))
+
+	// Init the watcher.
+	watcher := NewPartitionRingWatcher(testRingName, ringKey, store, logger, nil)
+	require.NoError(t, services.StartAndAwaitRunning(ctx, watcher))
+	t.Cleanup(func() {
+		require.NoError(t, services.StopAndAwaitTerminated(ctx, watcher))
+	})
+
+	// Init the handler.
+	editor := NewPartitionRingEditor(ringKey, store)
+	handler := NewPartitionRingPageHandler(watcher, editor)
+
+	t.Run("should fail if the partition ID is invalid", func(t *testing.T) {
+		data := url.Values{}
+		data.Set("action", "change_state")
+		data.Set("partition_id", "xxx")
+		data.Set("partition_state", PartitionActive.String())
+
+		req := httptest.NewRequest(http.MethodPost, "/partition-ring", strings.NewReader(data.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		recorder := httptest.NewRecorder()
+		handler.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		assert.Contains(t, recorder.Body.String(), "invalid partition ID")
+	})
+
+	t.Run("should fail if the partition does not exist", func(t *testing.T) {
+		data := url.Values{}
+		data.Set("action", "change_state")
+		data.Set("partition_id", "100")
+		data.Set("partition_state", PartitionActive.String())
+
+		req := httptest.NewRequest(http.MethodPost, "/partition-ring", strings.NewReader(data.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		recorder := httptest.NewRecorder()
+		handler.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		assert.Contains(t, recorder.Body.String(), ErrPartitionDoesNotExist.Error())
+	})
+
+	t.Run("should fail if the state is invalid", func(t *testing.T) {
+		data := url.Values{}
+		data.Set("action", "change_state")
+		data.Set("partition_id", "1")
+		data.Set("partition_state", "xxx")
+
+		req := httptest.NewRequest(http.MethodPost, "/partition-ring", strings.NewReader(data.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		recorder := httptest.NewRecorder()
+		handler.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		assert.Contains(t, recorder.Body.String(), "invalid partition state")
+	})
+
+	t.Run("should fail if the state change is not allowed", func(t *testing.T) {
+		data := url.Values{}
+		data.Set("action", "change_state")
+		data.Set("partition_id", "1")
+		data.Set("partition_state", PartitionPending.String())
+
+		req := httptest.NewRequest(http.MethodPost, "/partition-ring", strings.NewReader(data.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		recorder := httptest.NewRecorder()
+		handler.ServeHTTP(recorder, req)
+
+		assert.Equal(t, http.StatusBadRequest, recorder.Code)
+		assert.Contains(t, recorder.Body.String(), ErrPartitionStateChangeNotAllowed.Error())
+	})
+
+	t.Run("should succeed if the state change is allowed", func(t *testing.T) {
+		data := url.Values{}
+		data.Set("action", "change_state")
+		data.Set("partition_id", "1")
+		data.Set("partition_state", PartitionInactive.String())
+
+		req := httptest.NewRequest(http.MethodPost, "/partition-ring", strings.NewReader(data.Encode()))
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		recorder := httptest.NewRecorder()
+
+		// Pre-condition check.
+		require.Equal(t, PartitionActive, getPartitionStateFromStore(t, store, ringKey, 1))
+		require.Equal(t, PartitionActive, getPartitionStateFromStore(t, store, ringKey, 2))
+
+		handler.ServeHTTP(recorder, req)
+		assert.Equal(t, http.StatusFound, recorder.Code)
+
+		require.Equal(t, PartitionInactive, getPartitionStateFromStore(t, store, ringKey, 1))
+		require.Equal(t, PartitionActive, getPartitionStateFromStore(t, store, ringKey, 2))
+	})
 }

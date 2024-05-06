1c6fac9e24e32abbfce6590b9ed80dd98265b64b
Nick Pillitteri <nick.pillitteri@grafana.com>
2024-05-06 16:41:24 -0400

    cache: Remove duplicate cache interfaces and update implementations
    Remove the superfluous `cache.RemoteCacheClient` interface and change the
    names of methods on `cache.Cache` to be less generic.
    
    This is a hugely breaking change but all the breakage will be obvious
    compile errors. A search indicates that Mimir is the only database using
    the `cache.Cache` interface so breakage should be limited and
    straightforward to fix.
    
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- cache/versioned_test.go
+++ cache/versioned_test.go
@@ -15,12 +15,12 @@ func TestVersioned(t *testing.T) {
 		cache := NewMockCache()
 		v1 := NewVersioned(cache, 1)
 		data := map[string][]byte{"hit": []byte(`data`)}
-		v1.StoreAsync(data, time.Minute)
-		res := v1.Fetch(context.Background(), []string{"hit", "miss"})
+		v1.SetMultiAsync(data, time.Minute)
+		res := v1.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, data, res)
 		err := v1.Delete(context.Background(), "hit")
 		require.NoError(t, err)
-		res = v1.Fetch(context.Background(), []string{"hit", "miss"})
+		res = v1.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, map[string][]uint8{}, res)
 	})
 
@@ -28,25 +28,25 @@ func TestVersioned(t *testing.T) {
 		cache := NewMockCache()
 		v1 := NewVersioned(cache, 1)
 		v1Data := map[string][]byte{"hit": []byte(`first version`)}
-		v1.StoreAsync(v1Data, time.Minute)
+		v1.SetMultiAsync(v1Data, time.Minute)
 		v2 := NewVersioned(cache, 2)
 		v2Data := map[string][]byte{"hit": []byte(`second version`)}
-		v2.StoreAsync(v2Data, time.Minute)
+		v2.SetMultiAsync(v2Data, time.Minute)
 
-		resV1 := v1.Fetch(context.Background(), []string{"hit", "miss"})
+		resV1 := v1.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, v1Data, resV1)
-		resV2 := v2.Fetch(context.Background(), []string{"hit", "miss"})
+		resV2 := v2.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, v2Data, resV2)
 
 		err := v1.Delete(context.Background(), "hit")
 		require.NoError(t, err)
-		resV1 = v1.Fetch(context.Background(), []string{"hit", "miss"})
+		resV1 = v1.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, map[string][]uint8{}, resV1)
-		resV2 = v2.Fetch(context.Background(), []string{"hit", "miss"})
+		resV2 = v2.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, v2Data, resV2, "v2 cached data should still retain the data")
 		err = v2.Delete(context.Background(), "hit")
 		require.NoError(t, err)
-		resV2 = v2.Fetch(context.Background(), []string{"hit", "miss"})
+		resV2 = v2.GetMulti(context.Background(), []string{"hit", "miss"})
 		assert.Equal(t, map[string][]uint8{}, resV2)
 	})
 }

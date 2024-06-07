242d25564a04aa28e2082deaa208098c7f2a3b73
Nicolás Pazos <npazosmendez@gmail.com>
2024-06-07 12:44:48 -0300

    logging middleware: log `trace_id` only if sampled

--- middleware/logging.go
+++ middleware/logging.go
@@ -56,7 +56,7 @@ func NewLogMiddleware(log log.Logger, logRequestHeaders bool, logRequestAtInfoLe
 // logWithRequest information from the request and context as fields.
 func (l Log) logWithRequest(r *http.Request) log.Logger {
 	localLog := l.Log
-	traceID, ok := tracing.ExtractTraceID(r.Context())
+	traceID, ok := tracing.ExtractSampledTraceID(r.Context())
 	if ok {
 		localLog = log.With(localLog, "trace_id", traceID)
 	}

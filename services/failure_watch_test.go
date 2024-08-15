71405f2a1013ae74c6ba0ebf8a4e1e26c3db7a51
Peter Štibraný <pstibrany@gmail.com>
2024-08-15 12:44:56 +0200

    FailureWatcher can now be Closed, which will unregister all its listeners and close the channel.

--- services/failure_watch_test.go
+++ services/failure_watch_test.go
@@ -3,10 +3,12 @@ package services
 import (
 	"context"
 	"errors"
+	"fmt"
 	"testing"
 
 	e2 "github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/goleak"
 )
 
 func TestNilServiceFailureWatcher(t *testing.T) {
@@ -30,6 +32,9 @@ func TestNilServiceFailureWatcher(t *testing.T) {
 	require.Panics(t, func() {
 		w.WatchManager(m)
 	})
+
+	// Closing nil watcher doesn't panic.
+	w.Close()
 }
 
 func TestServiceFailureWatcher(t *testing.T) {
@@ -48,4 +53,73 @@ func TestServiceFailureWatcher(t *testing.T) {
 	e := <-w.Chan()
 	require.NotNil(t, e)
 	require.Equal(t, err, e2.Cause(e))
+}
+
+func TestServiceFailureWatcherClose(t *testing.T) {
+	s1 := serviceThatDoesntDoAnything()
+	s2 := serviceThatDoesntDoAnything()
+	m, err := NewManager(s1, s2)
+	require.NoError(t, err)
+
+	s3, errorsS3 := errorReturningService()
+	s4, errorsS4 := errorReturningService()
+
+	require.NoError(t, StartManagerAndAwaitHealthy(context.Background(), m))
+	require.NoError(t, StartAndAwaitRunning(context.Background(), s3))
+	require.NoError(t, StartAndAwaitRunning(context.Background(), s4))
+
+	// All goroutines created until now are unrelated to failure watcher.
+	defer goleak.VerifyNone(t, goleak.IgnoreCurrent(), goleak.Cleanup(func(_ int) {
+		require.NoError(t, StopManagerAndAwaitStopped(context.Background(), m))
+		// These services are expected to errors.
+		require.Error(t, StopAndAwaitTerminated(context.Background(), s3))
+		require.Error(t, StopAndAwaitTerminated(context.Background(), s4))
+
+		goleak.VerifyNone(t)
+	}))
+
+	// Watch manager and both services
+	w := NewFailureWatcher()
+	w.WatchManager(m)
+	w.WatchService(s3)
+	w.WatchService(s4)
+
+	ch := w.Chan()
+
+	// Verify that we receive error from s3.
+	err = fmt.Errorf("service3 error")
+	errorsS3 <- err
+
+	require.ErrorIs(t, <-ch, err)
+
+	// After closing failure watcher, we don't receive any more errors.
+	w.Close()
+	require.Nil(t, <-ch)
+
+	// Even sending error from service 4 doesn't make it to failure watcher.
+	errorsS4 <- fmt.Errorf("service4 error")
+	require.Nil(t, <-ch)
+
+	// Since watcher is now closed, it cannot be used to watch services or managers.
+	require.Panics(t, func() {
+		w.WatchService(s3)
+	})
+	require.Panics(t, func() {
+		w.WatchManager(m)
+	})
+}
+
+// Creates service which will return first error passed to the channel.
+func errorReturningService() (*BasicService, chan<- error) {
+	errCh := make(chan error)
+	return NewBasicService(nil, func(ctx context.Context) error {
+		for {
+			select {
+			case <-ctx.Done():
+				return nil
+			case e := <-errCh:
+				return e
+			}
+		}
+	}, nil), errCh
 }

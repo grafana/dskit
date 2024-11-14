424807d80463b4b1b7b41b60109ec99db4c58320
Nikos Angelopoulos <nikolaos.angelopoulos@grafana.com>
2024-11-14 18:36:32 +0100

    test(kv): create mock client for counting func calls

--- kv/mock.go
+++ kv/mock.go
@@ -2,9 +2,11 @@ package kv
 
 import (
 	"context"
+	"sync"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
+	"go.uber.org/atomic"
 )
 
 // The mockClient does not anything.
@@ -36,4 +38,79 @@ func (m mockClient) WatchKey(_ context.Context, _ string, _ func(interface{}) bo
 }
 
 func (m mockClient) WatchPrefix(_ context.Context, _ string, _ func(string, interface{}) bool) {
+}
+
+// MockCountingClient is a wrapper around the Client interface that counts the number of times its functions are called.
+// This is used for testing only.
+type MockCountingClient struct {
+	client Client
+
+	mtx   sync.Mutex
+	calls map[string]*atomic.Int32
+}
+
+func NewMockCountingClient(client Client) *MockCountingClient {
+	return &MockCountingClient{
+		client: client,
+		calls:  make(map[string]*atomic.Int32),
+	}
+}
+
+func (mc *MockCountingClient) List(ctx context.Context, prefix string) ([]string, error) {
+	mc.incCall("List")
+
+	return mc.client.List(ctx, prefix)
+}
+func (mc *MockCountingClient) Get(ctx context.Context, key string) (interface{}, error) {
+	mc.incCall("Get")
+
+	return mc.client.Get(ctx, key)
+}
+
+func (mc *MockCountingClient) Delete(ctx context.Context, key string) error {
+	mc.incCall("Delete")
+
+	return mc.client.Delete(ctx, key)
+}
+
+func (mc *MockCountingClient) CAS(ctx context.Context, key string, f func(in interface{}) (out interface{}, retry bool, err error)) error {
+	mc.incCall("CAS")
+
+	return mc.client.CAS(ctx, key, f)
+}
+
+func (mc *MockCountingClient) WatchKey(ctx context.Context, key string, f func(interface{}) bool) {
+	mc.incCall("WatchKey")
+
+	mc.client.WatchKey(ctx, key, f)
+}
+
+func (mc *MockCountingClient) WatchPrefix(ctx context.Context, key string, f func(string, interface{}) bool) {
+	mc.incCall("WatchPrefix")
+
+	mc.client.WatchPrefix(ctx, key, f)
+}
+
+func (mc *MockCountingClient) GetCalls(name string) int {
+	mc.mtx.Lock()
+	defer mc.mtx.Unlock()
+	count, exists := mc.calls[name]
+
+	if !exists {
+		return 0
+	}
+
+	return int(count.Load())
+}
+
+func (mc *MockCountingClient) incCall(name string) {
+	mc.mtx.Lock()
+	defer mc.mtx.Unlock()
+
+	if _, exists := mc.calls[name]; !exists {
+		mc.calls[name] = atomic.NewInt32(0)
+	}
+
+	value := mc.calls[name]
+	value.Add(1)
 }

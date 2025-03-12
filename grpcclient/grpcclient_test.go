dba807756693e7a6f1e40120ea59b05df0ba1335
Yuri Nikolic <durica.nikolic@grafana.com>
2025-03-12 16:59:51 +0100

    Force ClusterUnaryClientInterceptor to be the last one sent to grpc.WithChainUnaryInterceptor
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

--- grpcclient/grpcclient_test.go
+++ grpcclient/grpcclient_test.go
@@ -2,8 +2,11 @@ package grpcclient
 
 import (
 	"flag"
+	"fmt"
 	"testing"
 
+	"github.com/grafana/dskit/clusterutil"
+	"github.com/grafana/dskit/middleware"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 )
@@ -45,6 +48,67 @@ func TestConfig(t *testing.T) {
 	})
 }
 
-func TestDialOption(t *testing.T) {
-
+func TestDialOptionWithClusterValidation(t *testing.T) {
+	inputUnaryInterceptors, _ := Instrument(nil)
+	testCases := map[string]struct {
+		clusterValidation                     clusterutil.ClusterValidationConfig
+		rateLimit                             float64
+		inputUnaryInterceptors                []grpc.UnaryClientInterceptor
+		expectedUnaryInterceptors             int
+		expectedClusterUnaryClientInterceptor bool
+	}{
+		"if cluster validation label is not set we do not expect ClusterUnaryClientInterceptor": {
+			clusterValidation:                     clusterutil.ClusterValidationConfig{},
+			inputUnaryInterceptors:                inputUnaryInterceptors,
+			expectedUnaryInterceptors:             len(inputUnaryInterceptors),
+			expectedClusterUnaryClientInterceptor: false,
+		},
+		"if cluster validation label is set and there is no input and no implicit UnaryClientInterceptors we expect ClusterUnaryClientInterceptor to be the last one": {
+			clusterValidation:                     clusterutil.ClusterValidationConfig{Label: "cluster"},
+			inputUnaryInterceptors:                nil,
+			expectedUnaryInterceptors:             1,
+			expectedClusterUnaryClientInterceptor: true,
+		},
+		"if cluster validation label is set and there is no input and an implicit UnaryClientInterceptor we expect ClusterUnaryClientInterceptor to be the last one": {
+			clusterValidation:      clusterutil.ClusterValidationConfig{Label: "cluster"},
+			inputUnaryInterceptors: nil,
+			// setting rateLimit creates an implicit UnaryClientInterceptor
+			rateLimit:                             10,
+			expectedUnaryInterceptors:             2,
+			expectedClusterUnaryClientInterceptor: true,
+		},
+		"if cluster validation label is set and there are input and no implicit UnaryClientInterceptors we expect ClusterUnaryClientInterceptor to be the last one": {
+			clusterValidation:                     clusterutil.ClusterValidationConfig{Label: "cluster"},
+			inputUnaryInterceptors:                inputUnaryInterceptors,
+			expectedUnaryInterceptors:             len(inputUnaryInterceptors) + 1,
+			expectedClusterUnaryClientInterceptor: true,
+		},
+		"if cluster validation label is set and there are input and implicit UnaryClientInterceptors we expect ClusterUnaryClientInterceptor to be the last one": {
+			clusterValidation:      clusterutil.ClusterValidationConfig{Label: "cluster"},
+			inputUnaryInterceptors: inputUnaryInterceptors,
+			// setting rateLimit creates an implicit UnaryClientInterceptor
+			rateLimit:                             10,
+			expectedUnaryInterceptors:             len(inputUnaryInterceptors) + 2,
+			expectedClusterUnaryClientInterceptor: true,
+		},
+	}
+	for testName, testCase := range testCases {
+		t.Run(testName, func(t *testing.T) {
+			cfg := Config{}
+			cfg.ClusterValidation = testCase.clusterValidation
+			cfg.RateLimit = testCase.rateLimit
+			withChainUnaryInterceptor = func(unaryInterceptors ...grpc.UnaryClientInterceptor) grpc.DialOption {
+				require.Len(t, unaryInterceptors, testCase.expectedUnaryInterceptors)
+				if cfg.ClusterValidation.Label == "" {
+					require.Nil(t, cfg.clusterUnaryClientInterceptor)
+				} else {
+					require.NotNil(t, cfg.clusterUnaryClientInterceptor)
+					lastUnaryInterceptor := unaryInterceptors[len(unaryInterceptors)-1]
+					require.Equal(t, fmt.Sprintf("%p", cfg.clusterUnaryClientInterceptor), fmt.Sprintf("%p", lastUnaryInterceptor))
+				}
+				return grpc.WithChainUnaryInterceptor(unaryInterceptors...)
+			}
+			cfg.DialOption(testCase.inputUnaryInterceptors, nil, middleware.NoOpInvalidClusterValidationReporter)
+		})
+	}
 }

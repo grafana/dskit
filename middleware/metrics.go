1375d1bc13c7bb41bd61ffff236193302571a8ae
Arve Knudsen <arve.knudsen@gmail.com>
2025-04-16 15:34:25 +0200

    Update middleware/metrics.go
    Co-authored-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- middleware/metrics.go
+++ middleware/metrics.go
@@ -8,7 +8,7 @@ import (
 // NewInvalidClusterValidations registers and returns a new counter metric server_request_invalid_cluster_validation_labels_total.
 func NewInvalidClusterValidations(reg prometheus.Registerer) *prometheus.CounterVec {
 	return promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
-		Name: "server_request_invalid_cluster_validation_labels_total",
+		Name: "server_invalid_cluster_validation_label_requests_total",
 		Help: "Number of requests received by server with invalid cluster validation label.",
 	}, []string{"protocol", "method", "cluster_validation_label", "request_cluster_validation_label"})
 }

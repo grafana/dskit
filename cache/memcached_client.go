grafana/dskit

commit d08e866f3db87ff30645475c98917e4753480cf2

Add a configuration parameter to not enforce DNS Lookup on startup (#647)
* Add a configuration parameter to enforce DNS Lookup on startup and a Initialize() Method to decouple Client creation from initialization

* Make `dns_initialization_enabled` experimental

cache/memcached_client.go
@@ -124,6 +124,10 @@ type MemcachedClientConfig struct {
 
 	// TLS to use to connect to the Memcached server.
 	TLS dstls.ClientConfig `yaml:",inline"`
+
+	// DNSInitializationEnabled enables initial DNS lookup and background resolution on client creation.
+	// When false, Initialize() must be called manually.
+	DNSInitializationEnabled bool `yaml:"dns_initialization_enabled" category:"experimental"`
 }
 
 func (c *MemcachedClientConfig) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
@@ -141,6 +145,7 @@ func (c *MemcachedClientConfig) RegisterFlagsWithPrefix(prefix string, f *flag.F
 	f.IntVar(&c.MaxItemSize, prefix+"max-item-size", 1024*1024, "The maximum size of an item stored in memcached, in bytes. Bigger items are not stored. If set to 0, no maximum size is enforced.")
 	f.BoolVar(&c.TLSEnabled, prefix+"tls-enabled", false, "Enable connecting to Memcached with TLS.")
 	c.TLS.RegisterFlagsWithPrefix(prefix, f)
+	f.BoolVar(&c.DNSInitializationEnabled, prefix+"dns-initialization-enabled", true, "Enable initial DNS lookup and background resolution on memcached client creation.")
 }
 
 func (c *MemcachedClientConfig) Validate() error {
@@ -235,7 +240,18 @@ func NewMemcachedClientWithConfig(logger log.Logger, name string, config Memcach
 	if reg != nil {
 		reg = prometheus.WrapRegistererWith(prometheus.Labels{labelCacheName: name}, reg)
 	}
-	return newMemcachedClient(logger, client, selector, config, reg, name)
+	mcClient, err := newMemcachedClient(logger, client, selector, config, reg, name)
+	if err != nil {
+		return nil, err
+	}
+
+	if config.DNSInitializationEnabled {
+		if err := mcClient.Initialize(); err != nil {
+			return nil, err
+		}
+	}
+
+	return mcClient, nil
 }
 
 func newMemcachedClient(
@@ -291,15 +307,17 @@ func newMemcachedClient(
 		func() float64 { return 1 },
 	)
 
-	// As soon as the client is created it must ensure that memcached server
-	// addresses are resolved, so we're going to trigger an initial addresses
-	// resolution here.
-	if err := c.resolveAddrs(); err != nil {
-		return nil, err
-	}
+	return c, nil
+}
+
+// Initialize will start the background DNS resolution periodically and do an initial DNS resolution.
+// The background resolution goroutine is started even if the initial resolution fails.
+func (c *MemcachedClient) Initialize() error {
+	// Start the background DNS resolution
 	go c.resolveAddrsLoop()
 
-	return c, nil
+	// Do initial DNS resolution
+	return c.resolveAddrs()
 }
 
 func (c *MemcachedClient) Stop() {
@@ -740,10 +758,10 @@ func (c *MemcachedClient) resolveAddrs() error {
 	if err := c.addressProvider.Resolve(ctx, c.config.Addresses); err != nil {
 		level.Error(c.logger).Log("msg", "failed to resolve addresses for memcached", "addresses", strings.Join(c.config.Addresses, ","), "err", err)
 	}
-	// Fail in case no server address is resolved.
+	// Return error in case no server address is resolved.
 	servers := c.addressProvider.Addresses()
 	if len(servers) == 0 {
-		return fmt.Errorf("no server address resolved for %s", c.name)
+		return fmt.Errorf("no memcached server addresses were resolved")
 	}
 
 	return c.selector.SetServers(servers...)

cache/memcached_client_test.go
@@ -334,6 +334,59 @@ func BenchmarkMemcachedClient_sortKeysByServer(b *testing.B) {
 	}
 }
 
+func TestMemcachedClient_ServerDependency(t *testing.T) {
+	testCases := []struct {
+		name                     string
+		dnsInitializationEnabled bool
+	}{
+		{
+			name:                     "with DNS initialization enabled",
+			dnsInitializationEnabled: true,
+		},
+		{
+			name:                     "with DNS initialization disabled",
+			dnsInitializationEnabled: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			cfg := &MemcachedClientConfig{}
+			memcachedClientConfigDefaultValues(cfg)
+			cfg.Addresses = flagext.StringSliceCSV{"dns+invalid.:11211"}
+			cfg.DNSInitializationEnabled = tc.dnsInitializationEnabled
+
+			client, err := NewMemcachedClientWithConfig(
+				log.NewNopLogger(),
+				t.Name(),
+				*cfg,
+				prometheus.NewPedanticRegistry(),
+			)
+
+			if tc.dnsInitializationEnabled {
+				require.Nil(t, client)
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "no memcached server addresses were resolved")
+				return
+			}
+
+			if !tc.dnsInitializationEnabled {
+				require.NoError(t, err)
+				require.NotNil(t, client)
+
+				// Manually initialize the client but fail
+				err = client.Initialize()
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "no memcached server addresses were resolved")
+
+				// Verify that the client is still usable, even if initialization failed
+				res := client.GetMulti(context.Background(), []string{"some-key"})
+				require.Empty(t, res)
+			}
+		})
+	}
+}
+
 func setupDefaultMemcachedClient() (*MemcachedClient, *mockMemcachedClientBackend, error) {
 	backend := newMockMemcachedClientBackend()
 	client, err := newMemcachedClient(

commit 4a3248c0622c8f1b8554d214bfb5d5e8501f8650

feat: cache: fail client creation on DNS errors
This reverts the behavior introduced in #647. Ignoring errors during
start up is not a good idea and it has since caused an outage by
allowing object storage to be overloaded by badly configured caches/
store-gateways.

The problem meant to be solved by the original change (don't block
rollouts when memcached or DNS is unavailable) has since been solved
in different ways:
* We run 3 replicas of all cache types on Mimir, even metadata caches
* The DNS client used now always uses TCP and retries on errors

Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

cache/memcached_client.go
@@ -116,10 +116,6 @@ type MemcachedClientConfig struct {
 
 	// TLS to use to connect to the Memcached server.
 	TLS dstls.ClientConfig `yaml:",inline"`
-
-	// DNSIgnoreStartupFailures allows the client to start even if initial DNS resolution fails.
-	// When true, DNS failures are logged but client creation succeeds.
-	DNSIgnoreStartupFailures bool `yaml:"dns_ignore_startup_failures" category:"experimental"`
 }
 
 func (c *MemcachedClientConfig) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
@@ -135,7 +131,6 @@ func (c *MemcachedClientConfig) RegisterFlagsWithPrefix(prefix string, f *flag.F
 	f.IntVar(&c.MaxItemSize, prefix+"max-item-size", 1024*1024, "The maximum size of an item stored in memcached, in bytes. Bigger items are not stored. If set to 0, no maximum size is enforced.")
 	f.BoolVar(&c.TLSEnabled, prefix+"tls-enabled", false, "Enable connecting to Memcached with TLS.")
 	c.TLS.RegisterFlagsWithPrefix(prefix, f)
-	f.BoolVar(&c.DNSIgnoreStartupFailures, prefix+"dns-ignore-startup-failures", true, "Allow client creation even if initial DNS resolution fails.")
 }
 
 func (c *MemcachedClientConfig) Validate() error {
@@ -231,7 +226,7 @@ func NewMemcachedClientWithConfig(logger log.Logger, name string, config Memcach
 	go mcClient.resolveAddrsLoop()
 
 	// Do initial DNS resolution
-	if err := mcClient.resolveAddrs(); err != nil && !config.DNSIgnoreStartupFailures {
+	if err = mcClient.resolveAddrs(); err != nil {
 		mcClient.Stop()
 		return nil, err
 	}

cache/memcached_client_test.go
@@ -15,8 +15,6 @@ import (
 	"github.com/grafana/gomemcache/memcache"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/stretchr/testify/require"
-
-	"github.com/grafana/dskit/flagext"
 )
 
 func TestMemcachedClient_SetAsync(t *testing.T) {
@@ -387,54 +385,6 @@ func BenchmarkMemcachedClient_sortKeysByServer(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		client.sortKeysByServer(keys)
-	}
-}
-
-func TestMemcachedClient_ServerDependency(t *testing.T) {
-	testCases := []struct {
-		name                     string
-		dnsIgnoreStartupFailures bool
-	}{
-		{
-			name:                     "with DNS failures not ignored",
-			dnsIgnoreStartupFailures: false,
-		},
-		{
-			name:                     "with DNS failures ignored",
-			dnsIgnoreStartupFailures: true,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			cfg := &MemcachedClientConfig{}
-			memcachedClientConfigDefaultValues(cfg)
-			cfg.Addresses = flagext.StringSliceCSV{"dns+invalid.:11211"}
-			cfg.DNSIgnoreStartupFailures = tc.dnsIgnoreStartupFailures
-
-			client, err := NewMemcachedClientWithConfig(
-				log.NewNopLogger(),
-				t.Name(),
-				*cfg,
-				prometheus.NewPedanticRegistry(),
-			)
-
-			if !tc.dnsIgnoreStartupFailures {
-				require.Nil(t, client)
-				require.Error(t, err)
-				require.Contains(t, err.Error(), "no memcached server addresses were resolved")
-				return
-			}
-
-			if tc.dnsIgnoreStartupFailures {
-				require.NoError(t, err)
-				require.NotNil(t, client)
-
-				// Verify that the client is still usable, even if initialization failed
-				res := client.GetMulti(context.Background(), []string{"some-key"})
-				require.Empty(t, res)
-			}
-		})
 	}
 }
 
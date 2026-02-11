f4b3e490463871786087c0de252c562de253b92e
Julius Hinze <julius.hinze@grafana.com>
2026-02-11 17:22:07 +0100

    feat(tenant): use tenant metadata instead of subtenant

--- tenant/resolver.go
+++ tenant/resolver.go
@@ -2,6 +2,7 @@ package tenant
 
 import (
 	"context"
+	"fmt"
 	"strings"
 
 	"github.com/grafana/dskit/user"
@@ -12,11 +13,11 @@ import (
 // tenant ID. It returns an error user.ErrNoOrgID if there is no tenant ID
 // supplied or user.ErrTooManyOrgIDs if there are multiple tenant IDs present.
 //
-// If the orgID contains a subtenant (format "tenantID:subtenantID"), this function
-// strips the subtenant part and returns only the tenant ID. This ensures backward
-// compatibility with existing code that is not subtenant-aware.
+// If the orgID contains metadata (format "tenantID:key=value"), this function
+// strips the metadata part and returns only the tenant ID. This ensures backward
+// compatibility with existing code that is not metadata-aware.
 //
-// The SubtenantID is not validated.
+// The metadata is not validated.
 //
 // ignore stutter warning
 //
@@ -28,13 +29,13 @@ func TenantID(ctx context.Context) (string, error) {
 		return "", err
 	}
 	orgID, remaining, hasMoreIDs := stringsCut(orgIDs, tenantIDsSeparator)
-	tenantID := trimSubtenantID(orgID)
+	tenantID := trimMetadata(orgID)
 	if err := ValidTenantID(tenantID); err != nil {
 		return "", err
 	}
 	for hasMoreIDs {
 		orgID, remaining, hasMoreIDs = stringsCut(remaining, tenantIDsSeparator)
-		if tenantID != trimSubtenantID(orgID) {
+		if tenantID != trimMetadata(orgID) {
 			return "", user.ErrTooManyOrgIDs
 		}
 	}
@@ -45,12 +46,12 @@ func TenantID(ctx context.Context) (string, error) {
 // normalized list of ordered and distinct tenant IDs (as produced by
 // NormalizeTenantIDs).
 //
-// If the orgID contains subtenants (format "tenantID:subtenantID" or
-// "tenant1:sub1|tenant2:sub2"), this function strips the subtenant parts
+// If the orgID contains metadata (format "tenantID:key=value" or
+// "tenant1:k=v1|tenant2:k=v2"), this function strips the metadata parts
 // and returns only the tenant IDs. This ensures backward compatibility
-// with existing code that is not subtenant-aware.
+// with existing code that is not metadata-aware.
 //
-// SubtenantIDs are not validated.
+// Metadata is not validated.
 //
 // ignore stutter warning
 //
@@ -67,7 +68,7 @@ func TenantIDs(ctx context.Context) ([]string, error) {
 func parseTenantIDs(orgID string) ([]string, error) {
 	orgIDs := strings.Split(orgID, string(tenantIDsSeparator))
 	for i, part := range orgIDs {
-		tenantId := trimSubtenantID(part)
+		tenantId := trimMetadata(part)
 		if err := ValidTenantID(tenantId); err != nil {
 			return nil, err
 		}
@@ -76,69 +77,68 @@ func parseTenantIDs(orgID string) ([]string, error) {
 	return NormalizeTenantIDs(orgIDs), nil
 }
 
-// SubtenantID returns the subtenant ID from the context, or an empty string if
-// no subtenant is present. The orgID format is "tenantID:subtenantID" (e.g., "123456:k6").
+// TenantWithMetadata returns the tenant ID and optional metadata from the context, or an empty string if
+// no metadata is present. The orgID format is "tenantID:metadata" (e.g., "123456:test=yes").
 //
 //nolint:revive
-func SubtenantID(ctx context.Context) (tenantID string, subtenantID string, _ error) {
+func TenantWithMetadata(ctx context.Context) (tenantID string, metadata *Metadata, _ error) {
 	//lint:ignore faillint wrapper around upstream method
 	orgIDs, err := user.ExtractOrgID(ctx)
 	if err != nil {
-		return "", "", err
+		return "", nil, err
 	}
+	return ParseTenantWithMetadata(orgIDs)
+}
 
+// ParseTenantWithMetadata returns the tenant ID and optional metadata from
+// orgID(s). Metadata is nil if no metadata is present. The orgID format is
+// "tenantID:metadata". The metadata string must contain alphanumeric characters
+// and "-" or "=". Metadata is a key-value pair separated by "=".
+//
+// Examples:
+// - 123456 (tenantID=12345, metadata=<nil>)
+// - 123456:a=b (tenantID=12345, metadata={a: b})
+// - 123456:a (tenantID=12345, metadata={a: b})
+func ParseTenantWithMetadata(orgIDs string) (string, *Metadata, error) {
 	orgID, remaining, hasMoreIDs := stringsCut(orgIDs, tenantIDsSeparator)
-	tenantID, subtenantID = splitTenantAndSubtenant(orgID)
+	tenantID, metadataString := splitTenantAndMetadata(orgID)
 	if err := ValidTenantID(tenantID); err != nil {
-		return "", "", err
+		return "", nil, err
 	}
-	if err := ValidSubtenantID(subtenantID); err != nil {
-		return "", "", err
+	if err := ValidMetadata(metadataString); err != nil {
+		return "", nil, err
 	}
 	var nextOrgID string
 	for hasMoreIDs {
 		nextOrgID, remaining, hasMoreIDs = stringsCut(remaining, tenantIDsSeparator)
-		// We can compare the entire orgID, no need to split into tenant/subtenant.
+		// We can compare the entire orgID, no need to split into tenant/metadata.
 		// The orgID is already guaranteed to be valid.
 		if orgID != nextOrgID {
-			return "", "", user.ErrTooManyOrgIDs
+			return "", nil, user.ErrTooManyOrgIDs
 		}
 	}
-	return tenantID, subtenantID, nil
-}
-
-// SubtenantIDs returns a normalized list of all subtenant IDs from the context
-// (as produced by NormalizeTenantIDs). Empty subtenants are omitted from the
-// result.
-//
-// ignore stutter warning
-//
-//nolint:revive
-func SubtenantIDs(ctx context.Context) ([]string, error) {
-	//lint:ignore faillint wrapper around upstream method
-	orgID, err := user.ExtractOrgID(ctx)
+	md, err := parseMetadata(metadataString)
 	if err != nil {
-		return nil, err
+		return "", nil, err
 	}
-	return parseSubtenantIDs(orgID)
+	return tenantID, md, nil
 }
 
-func parseSubtenantIDs(orgID string) ([]string, error) {
-	parts := strings.Split(orgID, string(tenantIDsSeparator))
-	var subtenantIDs []string
-	for _, part := range parts {
-		tenantID, subtenantID := splitTenantAndSubtenant(part)
-		if err := ValidTenantID(tenantID); err != nil {
-			return nil, err
-		}
-		if subtenantID != "" {
-			if err := ValidSubtenantID(subtenantID); err != nil {
-				return nil, err
-			}
-			subtenantIDs = append(subtenantIDs, subtenantID)
-		}
+func parseMetadata(input string) (*Metadata, error) {
+	if input == "" {
+		return nil, nil
 	}
-	return NormalizeTenantIDs(subtenantIDs), nil
+	key, value, ok := stringsCut(input, '=')
+	if !ok {
+		return nil, fmt.Errorf("invalid metadata string: %s", input)
+	}
+	if strings.IndexByte(value, '=') != -1 {
+		return nil, fmt.Errorf("metadata must contain exactly one '=': %s", input)
+	}
+	return &Metadata{
+		Key:   key,
+		Value: value,
+	}, nil
 }
 
 type Resolver interface {
@@ -153,14 +153,9 @@ type Resolver interface {
 	// NormalizeTenantIDs).
 	TenantIDs(context.Context) ([]string, error)
 
-	// SubtenantID returns the tenant ID and subtenant ID from the context.
-	// Returns empty subtenant if no subtenant is present.
-	// The orgID format is "tenantID:subtenantID".
-	SubtenantID(context.Context) (string, string, error)
-
-	// SubtenantIDs returns all subtenant IDs from the context. It should return
-	// a normalized list of ordered and distinct subtenant IDs.
-	SubtenantIDs(context.Context) ([]string, error)
+	// TenantWithMetadata returns the tenant ID and metadata from the context.
+	// The orgID format is "tenantID:metadata".
+	TenantWithMetadata(context.Context) (string, *Metadata, error)
 }
 
 type MultiResolver struct{}
@@ -183,10 +178,6 @@ func (t *MultiResolver) TenantIDs(ctx context.Context) ([]string, error) {
 	return TenantIDs(ctx)
 }
 
-func (t *MultiResolver) SubtenantID(ctx context.Context) (string, string, error) {
-	return SubtenantID(ctx)
-}
-
-func (t *MultiResolver) SubtenantIDs(ctx context.Context) ([]string, error) {
-	return SubtenantIDs(ctx)
+func (t *MultiResolver) TenantWithMetadata(ctx context.Context) (string, *Metadata, error) {
+	return TenantWithMetadata(ctx)
 }

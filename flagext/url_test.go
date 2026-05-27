1cff08109f02fa30b002899994f577f702539855
Bryan Boreham <bjboreham@gmail.com>
2026-05-27 14:17:19 +0100

    Resolve scanner complaint
    We can't do embedded newlines either.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- flagext/url_test.go
+++ flagext/url_test.go
@@ -80,8 +80,7 @@ func TestURLValueYAML(t *testing.T) {
 
 		var testStruct TestStruct
 		require.NoError(t, testStruct.URL.Set("http://username:password@google.com")) // trufflehog:ignore
-		expected := []byte(`url: http://username:xxxxx@google.com
-`) // trufflehog:ignore
+		expected := []byte("url: http://username:xxxxx@google.com\n")                 // trufflehog:ignore
 
 		actual, err := yaml.Marshal(testStruct)
 		require.NoError(t, err)
